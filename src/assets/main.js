da1ed4b4d53359603fdbb63ddbf7fc04f8ebb875
Kali Rogers <kalirogers@gmail.com>
2017-09-19 22:03:23 -0500

    Create setHiddenField function

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,23 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+
+
 }
 
-//implement new functions here
+function setHiddenFields() {
+  var randomNum = Math.floor(Math.random() * 1000);
+  console.log(randomNum);
+  var randomNumString = randomNum.toString();
+  console.log(typeof(randomNumString));
+  while(randomNumString.length < 4) {
+      randomNumString = "0" + randomNumString;
+      console.log(randomNumString);
+      }
+  answer.value = randomNumString;
+  attempt.value = 0;
+  console.log(attempt);
+  console.log(answer);
+}
+
+setHiddenFields();
